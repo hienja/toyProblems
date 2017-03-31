f5d48bf60908c074befcc50b83f2576fcfc02740
Hien Tran <aznketchup@yahoo.com>
2017-03-30 20:59:20 -0700

    Initialize and implement isomorphicString.js. -Find if two strings are isomorphic, they share the same formation pattern. -Compare the two string's patterns using objects.

---
+++ isomorphicString.js
@@ -0,0 +1,48 @@
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+For example,
+Given "egg", "add", return true.
+
+Given "foo", "bar", return false.
+
+Given "paper", "title", return true.
+
+Note:
+You may assume both s and t have the same length.
+*/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+	var sPattern = {};
+	var tPattern = {};
+
+	for(var i = 0; i < s.length; i++) {
+		if(sPattern[s[i]] === undefined) {
+			sPattern[s[i]] = i;
+		}
+	} 
+
+	for(var j = 0; j < t.length; j++) {
+		if(tPattern[t[j]] === undefined) {
+			tPattern[t[j]] = j;
+		}
+		if(tPattern[t[j]] !== sPattern[s[j]]) {
+			return false;
+		}
+	}
+
+	return true;
+};
+
+console.log(isIsomorphic("egg", "add"))
+console.log(isIsomorphic("foo", "bar"))
+console.log(isIsomorphic("paper", "title"))
